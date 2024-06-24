c3fdbb1c63bc8157e4e76eb898099ade9c6546f9
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2024-06-24 18:03:15 -0400

    Update modeling_live.py

--- models/modeling_live.py
+++ models/modeling_live.py
@@ -197,7 +197,7 @@ def build_live(
     **kwargs
 ):
     quantization_config = BitsAndBytesConfig(load_in_4bit=True)
-    model = model_class.from_pretrained(llm_pretrained, config=config_class.from_pretrained(llm_pretrained, quantization_config=quantization_config, torch_dtype=torch.float16, **kwargs), torch_dtype=torch_dtype, quantization_config=quantization_config)
+    model = model_class.from_pretrained(llm_pretrained, config=config_class.from_pretrained(llm_pretrained, quantization_config=quantization_config, torch_dtype=torch.float16, device_map='auto', **kwargs), torch_dtype=torch_dtype, quantization_config=quantization_config)
     print("INFO: Loaded llm model")
     tokenizer = build_live_tokenizer_and_update_config(llm_pretrained, model.config)
     if is_training:
